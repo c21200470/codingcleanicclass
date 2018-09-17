acd486d12f5e04d10eed7f509cb6ec913402220c
jeff.sk.yoo <21200470>
2018-09-17 14:08:53 +0900

    HW1

--- app/src/main/java/a21200470/mycodingcleanic/MainActivity.java
+++ app/src/main/java/a21200470/mycodingcleanic/MainActivity.java
@@ -1,13 +1,32 @@
 package a21200470.mycodingcleanic;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView tv;
+    Button bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.txt);
+        bt = findViewById(R.id.button);
+
+        bt.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.button) {
+
+            tv.setText("Bye World!");
+        }
     }
 }
